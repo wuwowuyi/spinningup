09d310509c23ef364e0e0a0a3fa00756eb04dc1a
wuwowuyi <dalucheng@gmail.com>
2023-05-07 20:01:33 -0700

    upgrade plot code

--- spinup/run.py
+++ spinup/run.py
@@ -3,7 +3,7 @@ from spinup.user_config import DEFAULT_BACKEND
 from spinup.utils.run_utils import ExperimentGrid
 from spinup.utils.serialization_utils import convert_json
 import argparse
-import gym
+import gymnasium as gym
 import json
 import os, subprocess, sys
 import os.path as osp
